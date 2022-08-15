242877855ee8941e58b82a3adf1d468779187d8d
André Maiolini <andre.maiolini@hotmail.com>
2022-08-15 11:19:08 -0300

    Design da DepartmentList.fxml

---
+++ src/main/java/gui/DepartmentListController.java
@@ -0,0 +1,17 @@
+package gui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class DepartmentListController {
+
+    @FXML
+    private Button newBtn;
+
+    @FXML
+    protected void onNewButtonClick() {
+
+    }
+
+
+}
--- src/main/java/gui/MainViewController.java
+++ src/main/java/gui/MainViewController.java
@@ -37,7 +37,7 @@ public class MainViewController {
 
         @FXML
         protected void onMenuDepartmentAction() {
-            System.out.println("onMenuDepartmentAction");
+            loadView("departmentList.fxml");
         }
 
         @FXML
